923efacd27b7780036d1da0abd42e59e912b473e
Pablo Gaza <p.garza1983@gmail.com>
2024-02-04 22:08:53 +0100

    Add next-sanity package and fetch data from Sanity in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
+import { client } from "./lib/sanity";
 
-export default function Home() {
+async function getData() {
+const query = `
+*[_type == "blog"] | order(_createdAt desc) {
+  title,
+    smallDescription,
+    "currentSlug": slug.current
+}`;
+
+const data = await client.fetch(query);
+return data;
+
+}
+
+export default async function Home() {
+  const data = await getData();
+  console.log(data);
   return (
     <div>
       <h1>hello from the index page</h1>
