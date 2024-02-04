1aaef28cde230e2efdd368c24e184256ce8c8246
Pablo Gaza <p.garza1983@gmail.com>
2024-02-04 21:43:32 +0100

    Add Radix UI dropdown menu and Next Themes*** ***Update Navbar component and add ModeToggle*** ***Add ThemeProvider to RootLayout*** ***Update primary and ring colors in globals.css

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
+import { ModeToggle } from "./ModeToggle";
 
 export default function Navbar() {
     return (
         <nav className="w-full relative flex items-center justify-between max-w-3xl mx-auto p-4">
             <Link href="/" className="font-bold text-3xl">
-                Pablo <span className="text-blue-500">The Scarecrow</span> Garza
+                Pablo<span className="text-primary">The Scarecrow</span>Garza
             </Link>
+            <ModeToggle />
         </nav>
     );
     }
