89ed8792f255bd85fabcddff7e2a2e91791c5ceb
Pablo Gaza <p.garza1983@gmail.com>
2024-02-06 21:59:03 +0100

    Refactor blog page and update dependencies, add single blog content

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,50 @@
-export default function BlogArticle({params}: {params: {slug: string}}) {
-    return (
-        <div>
-            <h1>{params.slug}</h1>
-        </div>
-    );
+import { fullBlog } from "@/app/lib/interface";
+import { client, urlFor } from "@/app/lib/sanity";
+import { PortableText } from "@portabletext/react";
+import Image from "next/image";
+
+async function getData(slug: string) {
+  const query = `
+    *[_type == "blog" && slug.current == "${slug}"] {
+        "currentSlug": slug.current,
+        title,
+          content,
+          titleImage
+      } [0]`;
+
+  const data = await client.fetch(query);
+  return data;
+}
+
+export default async function BlogArticle({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const data: fullBlog = await getData(params.slug);
+  return (
+    <div className="p-8">
+      <h1>
+        <span className="text-base block text-primary text-center font-semibold tracking-wide uppercase">
+          Pablo Garza - Blog
+        </span>
+        <span className="pt-2 block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl">
+          {data.title}
+        </span>
+      </h1>
+
+      <Image
+        src={urlFor(data.titleImage).url()}
+        alt="Title image"
+        width={800}
+        height={800}
+        priority
+        className="rounded-lg mt-8 border w-full h-96"
+      />
+
+      <div className="mt-16 prose prose-blue prose-xl dark:prose-invert">
+        <PortableText value={data.content} />
+      </div>
+    </div>
+  );
 }
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <nav className="w-full relative flex items-center justify-between max-w-3xl mx-auto p-4">
             <Link href="/" className="font-bold text-3xl">
-                Pablo<span className="text-primary">The Scarecrow</span>Garza
+                The<span className="text-primary">Scarecrow</span>
             </Link>
             <ModeToggle />
         </nav>
