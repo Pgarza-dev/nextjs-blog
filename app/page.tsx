6a51f2a55f673bb17dbea393f288f96b1f5cf0a9
Pablo Gaza <p.garza1983@gmail.com>
2024-02-07 10:10:56 +0100

    Add revalidate constant for caching

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,10 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
+export const revalidate = 30; // revalidate at most every 30 seconds
+
 async function getData() {
-const query = `
+  const query = `
 *[_type == "blog"] | order(_createdAt desc) {
   title,
     smallDescription,
@@ -14,34 +16,35 @@ const query = `
     titleImage
 }`;
 
-const data = await client.fetch(query);
-return data;
-
+  const data = await client.fetch(query);
+  return data;
 }
 
 export default async function Home() {
   const data: simpleBlogCard[] = await getData();
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 mt-5 gap-5">
-    {data.map((post, idx) => (
-      <Card key={idx}>
-        <Image src={urlFor(post.titleImage).url()} alt="image" width={500} height={300} className="rounded-t-lg h-[300px] object-cover" />
-      
-     
-        <CardContent className="pt-5">
-          <h3 className="text-lg line-clamp-2 font-bold">{post.title}</h3>
-          <p className="line-clamp-3 text-sm pt-2 text-gray-600 dark:text-gray-300">{post.smallDescription}</p>
-        <Button asChild className="w-full mt-5">
-          <Link href={`/blog/${post.currentSlug}`}>
-            Read More
-          </Link>
-        </Button>
-        
-        </CardContent>
+      {data.map((post, idx) => (
+        <Card key={idx}>
+          <Image
+            src={urlFor(post.titleImage).url()}
+            alt="image"
+            width={500}
+            height={300}
+            className="rounded-t-lg h-[300px] object-cover"
+          />
 
-      </Card>))}
-
-
+          <CardContent className="pt-5">
+            <h3 className="text-lg line-clamp-2 font-bold">{post.title}</h3>
+            <p className="line-clamp-3 text-sm pt-2 text-gray-600 dark:text-gray-300">
+              {post.smallDescription}
+            </p>
+            <Button asChild className="w-full mt-5">
+              <Link href={`/blog/${post.currentSlug}`}>Read More</Link>
+            </Button>
+          </CardContent>
+        </Card>
+      ))}
     </div>
   );
 }
