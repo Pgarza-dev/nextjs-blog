093860eca5b8a082c06550c314f762122f6b7af3
Pablo Gaza <p.garza1983@gmail.com>
2024-02-12 20:52:54 +0100

    Add Embla Carousel React package

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,13 @@ import { client, urlFor } from "./lib/sanity";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export const revalidate = 30; // revalidate at most every 30 seconds
 
@@ -23,28 +30,55 @@ async function getData() {
 export default async function Home() {
   const data: simpleBlogCard[] = await getData();
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 mt-5 gap-5">
-      {data.map((post, idx) => (
-        <Card key={idx}>
-          <Image
-            src={urlFor(post.titleImage).url()}
-            alt="image"
-            width={500}
-            height={300}
-            className="rounded-t-lg h-[300px] object-cover"
-          />
+    <Carousel
+      opts={{
+        align: "start",
+      }}
+      className="w-full max-w-3xl">
+      <CarouselContent>
+        {data.map((post, index) => (
+          <CarouselItem key={index} className="md:basis-auto ">
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  {/* <span className="text-3xl font-semibold">{index + 1}</span> */}
+                  <Card key={index}>
+                    <Image
+                      src={urlFor(post.titleImage).url()}
+                      alt="image"
+                      width={500}
+                      height={300}
+                      className="rounded-t-lg h-[300px] object-cover"
+                    />
 
-          <CardContent className="pt-5">
-            <h3 className="text-lg line-clamp-2 font-bold">{post.title}</h3>
-            <p className="line-clamp-3 text-sm pt-2 text-gray-600 dark:text-gray-300">
-              {post.smallDescription}
-            </p>
-            <Button asChild className="w-full mt-5">
-              <Link href={`/blog/${post.currentSlug}`}>Read More</Link>
-            </Button>
-          </CardContent>
-        </Card>
-      ))}
-    </div>
+                    <CardContent className="pt-5">
+                      <h3 className="text-lg line-clamp-2 font-bold">
+                        {post.title}
+                      </h3>
+                      <p className="line-clamp-3 text-sm pt-2 text-gray-600 dark:text-gray-300">
+                        {post.smallDescription}
+                      </p>
+                      <Button asChild className="w-full mt-5">
+                        <Link href={`/blog/${post.currentSlug}`}>
+                          Read More
+                        </Link>
+                      </Button>
+                    </CardContent>
+                  </Card>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
   );
+}
+
+{
+  /* <div className="grid grid-cols-1 md:grid-cols-2 mt-5 gap-5">
+
+</div> */
 }
